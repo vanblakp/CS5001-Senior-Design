3315e78866c366f49f7884f046681de375d466db
HenryStiefel <hstiefel4@gmail.com>
2023-02-23 08:40:54 -0500

    Mexican Soldier animation tree and script

--- Assets/Henry/Characters/Mexican Soldier/MexicanSoldier.cs
+++ Assets/Henry/Characters/Mexican Soldier/MexicanSoldier.cs
@@ -4,15 +4,77 @@ using UnityEngine;
 
 public class MexicanSoldier : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public float checkRadius;
+    public float attackRadius;
+
+    public bool shouldRotate;
+
+    public LayerMask whatIsPlayer;
+
+    private Transform target;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Vector2 movement;
+    public Vector3 direction;
+
+    private bool isInChaseRange;
+    private bool isInAttackRange;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        animator.SetBool("isRunning", isInChaseRange);
+
+        isInChaseRange = Physics2D.OverlapCircle(transform.position, checkRadius, whatIsPlayer);
+        isInAttackRange = Physics2D.OverlapCircle(transform.position, attackRadius, whatIsPlayer);
+        direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        direction.Normalize();
+        movement = direction;
+
+        if (shouldRotate)
+        {
+            animator.SetFloat("X", direction.x);
+            animator.SetFloat("Y", direction.y);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float x = movement.x;
+        float y = movement.y;
+
+        direction = new Vector3(x, y, 0);
+
+        if (direction.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (direction.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 0);
+        }
+
         
+        if (isInChaseRange && !isInAttackRange)
+        {
+            MoveCharacter(movement);
+        }
+        if (isInAttackRange)
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void MoveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
     }
 }
