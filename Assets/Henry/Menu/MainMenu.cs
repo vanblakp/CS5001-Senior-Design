bb95aed2940f6fcc7ea268f8526a67e52c614166
HenryStiefel <hstiefel4@gmail.com>
2023-03-02 09:50:22 -0500

    Starting on main menu
    Working on Main Menu mockup with working buttons, import music from Old West graphics pack

---
+++ Assets/Henry/Menu/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+
+        /*
+         * Load the next level
+         * Must add the scenes to the queue for this to work
+         * File > Build Settings
+         * Add Menu scene, then add subsequent game scenes
+         */
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit the game successfully");
+        Application.Quit();
+    }
+}
