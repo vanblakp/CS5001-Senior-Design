8715d775dcd4ced86998b3798984a3dc30f914e1
Eric Michalski <emichalski2000@hotmail.com>
2023-03-15 16:54:38 -0400

    Moved some folders and fixed merger issues

--- Assets/Kyle/Scripts/Player.cs
+++ Assets/Kyle/Scripts/Player.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Mover
+public class Player : MonoBehaviour // Mover
 {
     private void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        UpdateMotor(new Vector3(x, y, 0));
+        //UpdateMotor(new Vector3(x, y, 0));
     }
 }
